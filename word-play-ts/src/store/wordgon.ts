04b0bafd006322e7722a174cf6a78d546b306bab
Jason Chew <jrchew15@gmail.com>
2023-03-27 21:23:56 -0700

    wordgon sessions state filled, untested

--- word-play-ts/src/store/wordgon.ts
+++ word-play-ts/src/store/wordgon.ts
@@ -1,10 +1,82 @@
-import { createSlice, Slice } from "@reduxjs/toolkit";
-import { wordgonPlaceholder } from "../classes/types";
+import { createSlice, PayloadAction, Slice } from "@reduxjs/toolkit";
+import { Dispatch } from "react";
+import { errorResponse, wordgonPlaceholder, wordgonSession } from "../classes/types";
 
 const wordgonSlice: Slice = createSlice({
     name: 'wordgon',
     initialState: { ...wordgonPlaceholder },
-    reducers: {}
+    reducers: {
+        setWordgonSession(state, action: PayloadAction<wordgonSession>): void {
+            state.id = action.payload;
+        },
+        loadWordgonSessions(state, action: PayloadAction<{ [id: number]: wordgonSession }>): void {
+            for (let key in action.payload) {
+                state[key] = action.payload[key]
+            }
+        },
+        deleteWordgonSession(state, action: PayloadAction<number>): void {
+            delete state[action.payload]
+        }
+    }
 });
 
 export default wordgonSlice.reducer;
+export const { setWordgonSession, loadWordgonSessions, deleteWordgonSession } = wordgonSlice.actions;
+
+
+export const thunkAddWordgonSession = (puzzleId: number) => async (dispatch: Dispatch<PayloadAction<wordgonSession>>) => {
+    const res = await fetch(`/api/wordgons/${puzzleId}/sessions`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+    })
+    if (res.ok) {
+        const data = await res.json() as wordgonSession;
+        await dispatch(setWordgonSession(data))
+        return data
+    }
+    const errData = await res.json() as errorResponse
+    return errData
+}
+
+export const thunkUpdateWordgonSession = (payload: { puzzleId: number, sessionId: number, guesses: string[], completed: boolean }) => async (dispatch: Dispatch<PayloadAction<wordgonSession>>) => {
+    const parsedPayload = {
+        guesses: payload.guesses.join(','),
+        numGuesses: payload.guesses.length,
+        completed: payload.completed
+    }
+    const res = await fetch(`/api/wordgons/${payload.puzzleId}/sessions/${payload.sessionId}`, {
+        method: 'PUT',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(parsedPayload)
+    });
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(setWordgonSession(data))
+        return data
+    }
+    const errData = await res.json() as errorResponse
+    return errData
+}
+
+export const thunkLoadWordgonSessions = () => async (dispatch: Dispatch<PayloadAction<{ [id: number]: wordgonSession }>>) => {
+    const res = await fetch(`/api/users/current/sessions`);
+
+    if (res.ok) {
+        const data = await res.json()
+        dispatch(loadWordgonSessions(data))
+        return null
+    }
+    const errData = await res.json() as errorResponse
+    return errData
+}
+
+export const thunkDeleteWordgonSession = (puzzleId: number, sessionId: number) => async (dispatch: Dispatch<PayloadAction<number>>) => {
+    const res = await fetch(`/api/wordgons/${puzzleId}/sessions/${sessionId}`, { method: 'DELETE' });
+
+    if (res.ok) {
+        dispatch(deleteWordgonSession(sessionId))
+        return
+    }
+    const data = await res.json()
+    return data
+}
